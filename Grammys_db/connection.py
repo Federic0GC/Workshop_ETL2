121bf2678a03cadc26e0c1be527cad9d3db20cff
Federico0GC <federico.gonzalez@uao.edu.co>
2024-04-21 16:07:47 -0500

    Code responsible for reading and uploading the  'grammys' dataframe to the database

--- Grammys_db/connection.py
+++ Grammys_db/connection.py
@@ -15,17 +15,15 @@ db_database = os.getenv("DB_DATABASE")
 try:
     Workshop_2_mysql_connection_str = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_database}'
     Workshop_2_mysql_db_connection = create_engine(Workshop_2_mysql_connection_str)
-    print("Conexión establecida con la base de datos...")
+    print("Connection established with the database...")
 
-    # Cargar el DataFrame en la base de datos
     grammy_awards.to_sql('grammy_awards', con=Workshop_2_mysql_db_connection, index=False, if_exists='replace')
-    print("Datos cargados correctamente en la tabla 'grammy_awards'.")
+    print("Data successfully loaded into the 'grammy_awards' table.")
 
 except pymysql.Error as e:
-    print(f"Fallo al conectar a la base de datos MySQL: {e}")
+    print(f"Failed to connect to MySQL database: {e}")
 
 finally:
-
     if 'Workshop_2_mysql_db_connection' in locals():
         Workshop_2_mysql_db_connection.dispose()
-    print("Conexión de datos y carga realizada correctamente, conexión cerrada.")
+    print("Data connection and loading completed successfully, connection closed.")
--- Grammys_db/grammy.py
+++ Grammys_db/grammy.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 Grammy_awards = 'Data/the_grammy_awards.csv' 
-grammy_awards = pd.read_csv(Grammy_awards)  # Elimina el argumento 'delimiter' para usar la coma como delimitador por defecto
+grammy_awards = pd.read_csv(Grammy_awards) 
 
 print(grammy_awards.head())
 print(grammy_awards.info())
